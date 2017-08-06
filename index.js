bc5eeb2511474b889ba33b8d562f13438cddfec8
Reginald Johnson <reginald.johnson@gmail.com>
2017-08-05 22:52:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+const util = require("util");
+
+// - provide access to aws-sdk
+//const AWS = require('aws-sdk');
+
+// - provide access to mysql databases
+// const mysql = require('mysql');
+
+// not sure what this is for
+// const parse = AWS.DynamoDB.Converter.output;
+
+// - used to create unique IDs. requires installation of uuid npm package
+// const uuidv1 = require('uuid/v1');
+
+// - set up a docClient to use with dynamoDB store
+/* let docClient = new AWS
+    .DynamoDB
+    .DocumentClient(); */
+
+// - access an environmental variable
+// let host = process.env.dbHost;
+
 exports.handler = (event, context, callback) => {
   const done = (err, success) => {
     callback(null, {
@@ -20,6 +42,12 @@ exports.handler = (event, context, callback) => {
   /****
    * Enter here
    */
+  // see the event that was received
+  console.log(
+    "fields: ",
+    util.inspect(event, { showHidden: false, depth: null })
+  );
+
   try {
     done(null, "you got an success");
   } catch (error) {
